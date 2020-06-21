0637ca56d40822dd2f67670e23c4eaf04ff3b9ad
rahul <rahul171627.cse@chitkara.edu.in>
2020-06-21 20:17:10 +0530

    send data

--- Controllers/PatientDatasController.cs
+++ Controllers/PatientDatasController.cs
@@ -88,6 +88,11 @@ namespace WNS_DEMOPROJECT.Controllers
             return View(patientData);
         }
 
+        public IActionResult Save(List<PatientData> pdt)
+        {
+            return Ok("success");
+        }
+
         // POST: PatientDatas/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
--- Models/PatientData.cs
+++ Models/PatientData.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace WNS_DEMOPROJECT.Models
 {
     public class PatientData
     {
         public int id { get; set; }
         public string assumptions { get; set; }
-
+        [Column(TypeName = "decimal(18, 2)")]
+        [Range(1, 1000)]
+        [DataType(DataType.Currency)]
         public decimal data { get; set; }
 
         public int year { get; set; }
